7d5dd0cffd31dbaa1f1af8869f6e043448f82b7e
Liam Tyler <liamtyler1010@gmail.com>
2024-10-12 10:54:05 -0400

    modified some django models

--- ppst/ppst/settings.py
+++ ppst/ppst/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
 ]
-AUTHENTICATION_BACKENDS = [
-    'website.backends.CustomUserBackend'
-]
+# AUTHENTICATION_BACKENDS = [
+#     'website.backends.CustomUserBackend'
+# ]
 
 
 MIDDLEWARE = [
