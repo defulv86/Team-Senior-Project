3920537ca8edcbb83610081b3356687751ead419
defulv86 <defulv86@students.rowan.edu>
2024-10-17 13:44:55 -0400

    Home Page HTML
    Developed the home page by modifying the HTML file and added image files to enhance visual appeal.
    Implemented sections for organizing content and added functionality for navigation to the login page via buttons.

--- ppst/website/urls.py
+++ ppst/website/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home/', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
