08a261362e1ccdc96d23a06d468747edfc93f6b3
Liam Tyler <liamtyler1010@gmail.com>
2024-10-21 21:08:06 -0400

    notification model added

--- ppst/website/models.py
+++ ppst/website/models.py
@@ -137,6 +137,3 @@ class Notification(models.Model):
     message = models.CharField(max_length=100)
     time_created = models.DateTimeField(auto_now=True)
     is_dismissed = models.BooleanField(default=False)
-
-    def  __str__(self):
-        return "Recipiant: " + self.user.username + " | Header: " + self.header + " | Message: " + self.message
